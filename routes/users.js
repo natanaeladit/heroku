7ece3a253b64cc89ed9e5e52af3ebc55237ef91a
Natanael Adityasatria <nata.adit@gmail.com>
2017-09-22 15:59:00 +0200

    show users

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Get all users');
+  res.send('Get all users and show them here');
 });
 
 module.exports = router;
