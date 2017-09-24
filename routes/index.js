4fb58654d6b9b47a8409003bb8fc6ff3356340b5
Natanael Adityasatria <nata.adit@gmail.com>
2017-09-24 20:03:44 +0200

    App

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Petaflop' });
+  res.render('index', { title: 'Petaflop app' });
 });
 
 module.exports = router;
